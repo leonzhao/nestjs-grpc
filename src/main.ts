d7c9f953d6b65d1a13d93376486b84d4cfae5cf7
leonzhao <yimingdream@gmail.com>
2018-06-21 12:19:39 +0800

    add eureka registry

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import Eureka from 'eureka-js-client' 
+// const Eureka = require('eureka-js-client').Eureka;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +17,35 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api/v1', app, document);
 
+  const eurekaClient = new Eureka({
+    instance: {
+      instanceId: 'nestjsService',
+      app: 'nestjsService',
+      hostName: 'localhost',
+      ipAddr: '192.168.0.153',
+      statusPageUrl: 'http://192.168.0.153:4000/info',
+      healthCheckUrl: 'http://192.168.0.153:4000/health',
+      port: {
+        '$': 4000,
+        '@enabled': true
+      },
+      vipAddress: 'nestjsService',
+      dataCenterInfo: {
+        '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+        name: 'MyOwn'
+      }
+    },
+    eureka: {
+      // eureka server host / port
+      fetchRegistry: false,
+      host: '192.168.0.47',
+      port: 8000,
+      servicePath: '/eureka/apps/'
+      // serviceUrl: 'http://192.168.0.47:8000/eureka/apps/'
+    },
+  })
+
+  eurekaClient.start((data) => console.log(data))
   await app.listen(4000);
 }
 bootstrap();
